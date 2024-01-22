548831dee7ed062746df080036a57115aed2e11a
KarolusPaulus <karosaarinenn@gmail.com>
2024-01-22 16:41:41 +0200

    vastattu

---
+++ teht4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "game.h"
+
+using namespace std;
+
+int main()
+{
+    game gameObject(40);
+    gameObject.play();
+
+    return 0;
+}
